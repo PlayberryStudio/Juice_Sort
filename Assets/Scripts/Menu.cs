04403f93366518082aa6be90524ae3b41272b507
MahmooD13841 <mahmod.1386.z@gmail.com>
2026-02-07 03:13:26 +0330

    Create Menu.cs

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+   //menu aval bazi  
+   public GameObject panel;
+   public void play()
+    {
+        
+    }   
+
+   public void settings()
+    {
+        panel.SetActive(true);
+    }   
+
+   public void survay()
+    {
+        
+    }   
+
+   
+}
